dc940d2ab759fafb9feef84a012f291541ec1c82
发光的半导体 <694014820@qq.com>
2021-07-25 03:18:16 +0800

    m

--- test/src/main/java/open/source/sharedscopecache/test/MainActivity.kt
+++ test/src/main/java/open/source/sharedscopecache/test/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val view = findViewById<ImageView>(R.id.image)
-        Glide.with(this).load(intent.getStringExtra("url")).into(view)
+        Glide.with(this).load(intent.getStringExtra("url"))
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .skipMemoryCache(true)
+            .into(view)
     }
 
     companion object {
