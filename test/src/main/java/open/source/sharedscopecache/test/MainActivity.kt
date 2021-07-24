214aea153949ca9b73085b7e2bb4508175fc4c36
发光的半导体 <694014820@qq.com>
2021-07-24 22:56:46 +0800

    m

--- test/src/main/java/open/source/sharedscopecache/test/MainActivity.kt
+++ test/src/main/java/open/source/sharedscopecache/test/MainActivity.kt
@@ -17,11 +17,13 @@ class MainActivity : AppCompatActivity() {
         val view = findViewById<View>(R.id.root)
         thread {
             val bytes =
-                SharedScopeCache.load(Uri.parse("content://open.source.sharedscopecache.sample.shared_scope_cache?key=547bb470e76c284b891360bcb75305e4422210b93122e17e1ccd644f520937d0"))
+                SharedScopeCache.load(Uri.parse("content://open.source.sharedscopecache.sample.shared_scope_cache?key=0d2785b50f9ff108f9632ef37255d6f767721075d33af9aa483e9ecbdf83af7b"))
             Log.d(TAG, "load=" + (bytes?.size ?: 0))
             if (bytes != null) {
-                view.background =
-                    BitmapDrawable(BitmapFactory.decodeByteArray(bytes, 0, bytes.size))
+                runOnUiThread {
+                    view.background =
+                        BitmapDrawable(BitmapFactory.decodeByteArray(bytes, 0, bytes.size))
+                }
             }
         }
     }
